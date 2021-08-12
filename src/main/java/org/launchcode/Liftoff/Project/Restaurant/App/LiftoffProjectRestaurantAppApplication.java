6357b5eda2e0d016e104541e699b0c2ca84fa53b
Andrew <narflz@gmail.com>
2021-08-12 15:47:48 -0500

    Used Spring to create baseline project. Included basic dependencies (boot-devtools and starter-web) and set the whole project up with Gradle. Created an initial test controller that simply displays "Hello, World!" to verify the project boots.

---
+++ src/main/java/org/launchcode/Liftoff/Project/Restaurant/App/LiftoffProjectRestaurantAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Liftoff.Project.Restaurant.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffProjectRestaurantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffProjectRestaurantAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Liftoff/Project/Restaurant/App/LiftoffProjectRestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Liftoff.Project.Restaurant.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffProjectRestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
